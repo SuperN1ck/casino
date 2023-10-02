71518c09381118e42e5ea7431ce1caf832efc79f
Nick Heppert <heppert@cs.uni-freiburg.de>
2023-10-02 13:10:43 +0200

    Add `random.py`

---
+++ src/casino/random.py
@@ -0,0 +1,22 @@
+import random
+
+
+def set_seed(seed: int):
+    """
+    Sets seed for python as well as for torch and numpy if available
+    """
+    random.seed(seed)
+
+    try:
+        import torch
+
+        torch.manual_seed(seed)
+    except:
+        pass
+
+    try:
+        import numpy as np
+
+        np.random.seed(seed)
+    except:
+        pass
