2f9db7dc891033e5639590215eaba046c5ffb57a
Nick Heppert <heppert@cs.uni-freiburg.de>
2023-10-02 11:20:41 +0200

    Add `checkpoints.py`

---
+++ src/casino/learning/checkpoints.py
@@ -0,0 +1,55 @@
+import pathlib
+from typing import Any, Union, Tuple
+
+
+def save_torch_checkpoint(
+    experiment_directory: Union[pathlib.Path, Any],
+    model: "torch.nn.Module",
+    optimizer: "torch.optim.Optimizer",
+    epoch: int,
+    current_epoch: bool = False,
+    latest: bool = True,
+    best: bool = False,
+):
+    import torch
+
+    if not isinstance(experiment_directory, pathlib.Path):
+        experiment_directory = pathlib.Path(experiment_directory)
+    experiment_directory /= "checkpoints"
+    experiment_directory.mkdir(parents=True, exist_ok=True)
+
+    ckpt_names = []
+    if current_epoch:
+        ckpt_names.append(f"{epoch}")
+    if latest:
+        ckpt_names.append("latest")
+    if best:
+        ckpt_names.append("best")
+
+    for ckpt_name in ckpt_names:
+        torch.save(
+            {
+                "model_parameters": model.state_dict(),
+                "optimizer_parameters": optimizer.state_dict(),
+                "epoch": epoch,
+            },
+            experiment_directory / (ckpt_name + ".ckpt"),
+        )
+
+
+def load_torch_checkpoint(
+    experiment_directory: Union[pathlib.Path, Any], ckpt_name: str = "best"
+) -> Tuple["torch.nn.Module", "torch.optim.Optimizer", int]:
+    import torch
+
+    if not isinstance(experiment_directory, pathlib.Path):
+        experiment_directory = pathlib.Path(experiment_directory)
+    experiment_directory /= "checkpoints"
+    checkpoint_dict = torch.load(
+        experiment_directory / (ckpt_name + ".ckpt"),
+    )
+    return (
+        checkpoint_dict["model_parameters"],
+        checkpoint_dict["optimizer_parameters"],
+        checkpoint_dict["epoch"],
+    )
