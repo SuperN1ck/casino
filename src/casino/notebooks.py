f29088f7e74608a44420c54709eac43ee1fd4e46
Nick Heppert <heppert@cs.uni-freiburg.de>
2023-10-16 14:43:36 +0200

    Add `is_notebook`-function

---
+++ src/casino/notebooks.py
@@ -0,0 +1,14 @@
+def is_notebook() -> bool:
+    """
+    https://stackoverflow.com/a/39662359
+    """
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        elif shell == "TerminalInteractiveShell":
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
