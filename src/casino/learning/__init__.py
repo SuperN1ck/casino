251b09cab8f94ae07ee27983389cc50e1f5dc62f
Nick Heppert <heppert@cs.uni-freiburg.de>
2023-11-09 18:51:03 +0100

    Get default torch device

--- src/casino/learning/__init__.py
+++ src/casino/learning/__init__.py
@@ -1,3 +1,14 @@
 from . import checkpoints, default_init, learning_rate_schedulers
 
 __all__ = ["checkpoints", "default_init", "learning_rate_schedulers"]
+
+
+try:
+    import torch
+    DEFAULT_TORCH_DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+except:
+    import logging
+    logging.debug(
+        "torch not availble. DEFAULT_TORCH_DEVICE is not set"
+    )
+
