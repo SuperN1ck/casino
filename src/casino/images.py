e528c75c5931284ff24fd458ecb687221aa65dc5
Nick Heppert <heppert@cs.uni-freiburg.de>
2024-03-19 16:15:08 +0100

    Export images as video

--- src/casino/images.py
+++ src/casino/images.py
@@ -13,6 +13,10 @@ try:
 except:
     logging.debug("PIl not availble. Most functionality in images.py will break.")
 
+try:
+    import cv2
+except:
+    logging.debug("cv2 not availble. Most functionality in images.py will break.")
 
 
 def save_images_to_gif(
@@ -23,9 +27,12 @@ def save_images_to_gif(
 ):
     assert not (duration is None and fps is None)
 
+    if output_path.suffix == "":
+        output_path = output_path.with_suffix(".gif")
+
     if duration is None:
         duration = len(images) / fps
-    
+
     im_0 = Image.fromarray(images[0])
     im_0.save(
         pathlib.Path(output_path),
@@ -35,3 +42,29 @@ def save_images_to_gif(
         duration=duration,
         loop=0,
     )
+
+
+def save_images_to_mp4(
+    images: List["np.ndarray"],
+    output_path: Union[pathlib.Path, str],
+    fps: Optional[float] = None,
+    duration: Optional[float] = None,
+):
+    assert not (duration is None and fps is None)
+
+    if output_path.suffix == "":
+        output_path = output_path.with_suffix(".avi")
+
+    if fps is None:
+        fps = len(images) / duration
+
+    width, height, _ = images[0].shape
+    video = cv2.VideoWriter(
+        filename=str(output_path), fourcc=0, fps=fps, frameSize=(height, width)
+    )
+
+    for image in images:
+        video.write(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+
+    cv2.destroyAllWindows()
+    video.release()
