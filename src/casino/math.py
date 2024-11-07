b46e12098bd866ee48c698f65d4a5b3ee3a35ca0
Nick Heppert <heppert@cs.uni-freiburg.de>
2024-11-07 16:36:55 +0100

    Math helpers

--- src/casino/math.py
+++ src/casino/math.py
@@ -5,6 +5,16 @@ try:
 except:
     logging.debug("numpy not availble. Most functionality in math.py will break")
 
+    class np:
+        nan = float("nan")
+
+
+try:
+    import torch
+except:
+    logging.debug("torch not available. Some functionality in math.py will break")
+
+
 from typing import List, Union
 
 
@@ -33,3 +43,14 @@ def within_range(
 
 def multiply_along_axis(A: "np.ndarray", B: "np.ndarray", axis: int):
     return np.swapaxes(np.swapaxes(A, axis, -1) * B, -1, axis)
+
+
+def take_along_axis(
+    A: "np.ndarray", indices: "np.ndarray", oob_value: float = np.nan, axis: int = -1
+):
+    assert A.ndim - 1 == indices.shape[1]
+
+    if axis == -1:
+        axis = A.ndim - 1
+
+    raise NotImplementedError()
