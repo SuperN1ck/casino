77a654ece12d93d700fdb344e2987fd30cc30fba
Nick Heppert <heppert@cs.uni-freiburg.de>
2024-11-07 16:36:20 +0100

    Clear `hardware.py`

--- src/casino/hardware.py
+++ src/casino/hardware.py
@@ -1,13 +1,2 @@
-import gc
-
-try:
-    import torch
-except:
-    import logging
-
-    logging.debug("torch not availble. Some functionality in hardwear.py will break")
-
-
-def clear_torch_memory():
-    gc.collect()
-    torch.cuda.empty_cache()
+# Moved
+from .torch_utils import clear_torch_memory
