a833a4a9593afc65bc66426f9bc508496a828693
Nick Heppert <heppert@cs.uni-freiburg.de>
2025-09-16 17:01:24 +0200

    Add a function to return the explained variance for the `SVDProjector`

--- src/casino/latents.py
+++ src/casino/latents.py
@@ -8,6 +8,8 @@ except:
 
 class SVDProjector:
     def __init__(self, X: "np.ndarray", info: bool = True):
+        # Is this too similar to sklearn's PCA? 
+        # https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html#pca
         self.X_mean = X.mean(axis=0)
         self.U, self.S, self.Vh = np.linalg.svd(X - self.X_mean)
         relative_S = self.S / np.sum(self.S)
@@ -21,3 +23,11 @@ class SVDProjector:
     def project_to_higher(self, X: "np.ndarray", dim_: int = 2):
         return (X @ self.Vh.T[:, :dim_].T) + self.X_mean
         # return X @ Vh[:dim, :]
+
+    def explained_variances(self):
+        """Returns the explained variances for all dimensions."""
+        return np.cumsum(self.S) / np.sum(self.S)
+
+    def explained_variance(self, dim_: int = 2):
+        """Returns the explained variance for the first `dim_` dimensions."""
+        return np.sum(self.S[:dim_]) / np.sum(self.S)
